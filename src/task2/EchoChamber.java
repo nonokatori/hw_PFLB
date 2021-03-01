39f29bbb4113e23b30e972bb1bdd5d6250c0a025
rozhina_vn <nonotori.nt@gmail.com>
2021-03-01 12:07:00 +0300

    Initial commit

---
+++ src/task2/EchoChamber.java
@@ -0,0 +1,31 @@
+package task2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EchoChamber {
+
+    public static void main(String[] args) {
+        print(readBuffer());
+
+    }
+
+    public static ArrayList<String> readBuffer() {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        ArrayList<String> ls = new ArrayList<>();
+        while (true) {
+            s = scanner.nextLine();
+            if (s.equals("")) break;
+            ls.add(s);
+        }
+        scanner.close();
+        return ls;
+    }
+
+    public static void print(ArrayList<String> strings) {
+        for (int i = 0; i < strings.size(); i++) {
+            System.out.println(strings.get(i));
+        }
+    }
+}
---
+++ src/task3/IOChamber.java
@@ -0,0 +1,41 @@
+package task3;
+
+import task2.EchoChamber;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class IOChamber {
+
+    private static ArrayList<String> ls = new ArrayList<>();
+    public static void main(String[] args) {
+
+        if (args.length !=0) {
+            IOChamber.readFile(args[0]);
+        }
+        if (ls.size()==0) {
+            System.out.println("Файл не распознан. Введите ваши сообщения.");
+            ls = EchoChamber.readBuffer();
+        }
+        printOddLines();
+    }
+
+    public static void readFile (String path) {
+        String str;
+        try(FileReader fr = new FileReader(path); BufferedReader reader = new BufferedReader(fr)) {
+            while ((str = reader.readLine()) != null) {
+                ls.add(str);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не распознан. Введите ваши сообщения.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printOddLines () {
+        for (int i = 0; i < ls.size(); i++) {
+            if(i%2 == 0) System.out.println(ls.get(i));
+        }
+    }
+}
